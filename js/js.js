654cde387b41471201a7acc23d317372e723826c
Taiwei Zhang <taiwei426@gmail.com>
2018-01-13 19:36:57 -0800

    deleted pin, not necessary css & js files for pin, and other random files

--- js/js.js
+++ js/js.js
@@ -79,33 +79,6 @@ $(function(){
 	});
 
 
-	// //http://codepen.io/agelber/pen/rAuaf
-	// var currentIndex = 0,
-	// itemCount    = $('.carousel_image_container > li').length;
-
-	// /* add the active class to the first item to hide all the others */
-	// // $('.carousel_image_container > li:eq(' + currentIndex + ')').addClass('active');
-
-	// $('.carousel-nav').on('click', function() {
-	// 	var $active  = $('.carousel_image_container > li.active'),
-	// 			isNext   = $(this).hasClass('next');
-	// 	currentIndex = (currentIndex + (isNext ? 1 : -1)) % itemCount;
-
-	// 	/* go back to the last item if we hit -1 */
-	// 	if (currentIndex === -1) {
-	// 		currentIndex = itemCount - 1;
-	// 	}
-
-	// 	var $next = $('.carousel_image_container > li:eq(' + currentIndex + ')');
-	// 	$active.addClass(isNext ? 'next-out' : 'prev-out');
-	// 	$next.addClass('active').addClass(isNext ? 'next-in' : 'prev-in');
-	// 	setTimeout(function() { 
-	// 		$active.removeClass('active next-out prev-out');
-	// 		$next.removeClass('next-in prev-in')
-	// 	});
-	// 	return false;
-	// });
-
 
 })
 
